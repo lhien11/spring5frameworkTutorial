25b9ab6553837925b1fb70fa340200955716011e
Hien Le <lhien11@gmail.com>
2018-01-12 02:14:21 -0800

    Repository pattern for Book and Author

---
+++ src/main/java/hienle/springframework/spring5webapp/bootstrap/DevBootstrap.java
@@ -0,0 +1,52 @@
+package hienle.springframework.spring5webapp.bootstrap;
+
+import  hienle.springframework.spring5webapp.model.Author;
+import  hienle.springframework.spring5webapp.model.Book;
+import  hienle.springframework.spring5webapp.repositories.AuthorRepository;
+import  hienle.springframework.spring5webapp.repositories.BookRepository;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by hien le on 1/12/18
+ */
+
+@Component
+public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> {
+
+    private AuthorRepository authorRepository;
+    private BookRepository bookRepository;
+
+    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+    }
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+        initData();
+    }
+
+    private void initData() {
+        // Eric
+        Author eric = new Author("Eric", "Evans");
+        Book ddd = new Book("Domain Driven Design", "1234", "Harper Collins");
+        eric.getBooks().add(ddd);
+        ddd.getAuthors().add(eric);
+
+        authorRepository.save(eric);
+        bookRepository.save(ddd);
+
+
+        // Rod
+        Author rod = new Author("Rod", "Johnson");
+        Book noEJB = new Book("J2EE Development without EJB", "23444", "Worx");
+        rod.getBooks().add(noEJB);
+        authorRepository.save(rod);
+        bookRepository.save(noEJB);
+
+
+    }
+
+}
---
+++ src/main/java/hienle/springframework/spring5webapp/repositories/AuthorRepository.java
@@ -0,0 +1,10 @@
+package hienle.springframework.spring5webapp.repositories;
+
+import hienle.springframework.spring5webapp.model.Author;
+import org.springframework.data.repository.CrudRepository;
+
+
+public interface AuthorRepository extends CrudRepository<Author, Long> {
+
+}
+
